285ba443c11b58514d895223ba389387a0108ad7
kpeterson8 <38024285+kpeterson8@users.noreply.github.com>
2020-05-02 12:47:45 -0500

    About Us page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -12,10 +12,10 @@ import {
 } from '../../components';
 
 import css from './AboutPage.css';
-import image from './about-us-1056.jpg';
+import image from './Day Dreamer_about-us.jpg';
 
 const AboutPage = () => {
-  const { siteTwitterHandle, siteFacebookPage } = config;
+  const { siteTwitterHandle, siteFacebookPage, siteInstagramPage} = config;
   const siteTwitterPage = twitterPageURL(siteTwitterHandle);
 
   // prettier-ignore
@@ -25,7 +25,7 @@ const AboutPage = () => {
       schema={{
         '@context': 'http://schema.org',
         '@type': 'AboutPage',
-        description: 'About Yogatime',
+        description: 'About Day Dreamer',
         name: 'About page',
       }}
     >
@@ -35,51 +35,50 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Find new depths in your yoga practice</h1>
+          <h1 className={css.pageTitle}>We believe getting hired should be easy. Let's make it happen.</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Yoga was listed by UNESCO as an intangible cultural heritage.</p>
+              <p>Did you know that over 80% of creatives don't price themselves correctly?</p>
             </div>
 
             <div className={css.contentMain}>
               <h2>
-                Each yoga practitioner is an individual, and each one of us needs different care.
-                Working together with an experienced yoga teacher offers the possibility to rise
-                our practise to a whole new level.
+                We want to make getting hired easier, faster, and more fun! We've created a system for you - reviews,
+                rates, and a full-service hiring platform.
               </h2>
 
               <p>
-                Whether it is the alignment of asanas or being able to set the focus of the class,
-                we all have our own struggles and goals. Some of these cannot be addressed in a
-                regular class of twenty yogis. Working together with the experienced yoga teachers
-                from Yogatime, you can together create just the right class for you.
+                As a creative professional, you want to diversify your income, which starts with having a competitive
+                rate, reviews, and a payment system that protects you. With Day Dreamer, get booked easily, chat
+                either in the portal or mobile app, and receive your payment as soon as the gig is completed.
               </p>
 
-              <h3 className={css.subtitle}>Are you a yoga teacher?</h3>
+              <h3 className={css.subtitle}>So you want to make more money?</h3>
 
               <p>
-                Yogatime offers you a platform through which you can reach thousands of yoga
-                practitioners. Offering private yoga classes through Yogatime offers you a
-                possibility to grow your customer base and earn some extra income on top of your
-                regular classes.
+                Day Dreamer is made by and for creatives, but backed by data. We believe in empowering creative
+                professionals to stay competitive in their medium and local community. You can also easily import
+                your portfolio projects directly into Day Dreamer. No more uploading the same video, song, or photo
+                to multiple websites!
               </p>
 
               <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Yogatime
+                Are you looking to hire local creatives?
               </h3>
               <p>
-                Yogatime is brought to you by{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Sharetribe
-                offers anyone a possibility to create a marketplace without restricting your own
-                creativity. Do not hesitate to reach out and learn how to best turn your
-                marketplace idea to reality.
+                Day Dreamer has been helping local small businesses and other creatives hire locally since
+                2019 when we created our{' '}<ExternalLink href="https://medium.com/day-dreamer-app/dreamers-doers-a-new-way-to-hire-creatives-3de26228fc7e"
+                >Dreamers & Doers pilot</ExternalLink>, saving hirers 20-30% compared to a creative agency.
+                If you're looking for new and local, top talent, where you can save more time + money,
+                look no further - we got you!
               </p>
               <p>
-                You can also checkout our{' '}
-                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+                Stay in touch with us at{' '}
+                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink>{', '}
+                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>, and{' '}
+                <ExternalLink href={siteInstagramPage}>Instagram</ExternalLink>.
               </p>
             </div>
           </div>
